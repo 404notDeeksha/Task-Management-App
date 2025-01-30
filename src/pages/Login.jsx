e2fe0f524b6aa97d1cb63ddf33f649b167a2dd38
Deeksha_GitFiles <sharmadeekksha@gmail.com>
2025-01-30 21:05:17 +0530

    Tailwind working fine

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Login = () => {
+  return (
+    <div className="bg-blue-500 text-white text-3xl font-bold p-5">
+      Tailwind v4 is working! 🎉
+    </div>
+  );
+};
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <>
+      <h3>NotFound</h3>
+      <h4>Error: 404</h4>
+    </>
+  );
+};
