792a2df36f0426853be430c6c2128a5bcdd71aba
Deeksha_GitFiles <sharmadeekksha@gmail.com>
2025-02-03 00:49:58 +0530

    updated protected route import

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,7 +4,7 @@ import { Login } from "../pages/Login";
 import { Signup } from "../pages/Signup";
 import { NotFound } from "../pages/NotFound";
 import { Dashboard } from "../pages/Dashboard";
-import { ProtectedRoute } from "../components/ProtectedRoute    ";
+import { ProtectedRoute } from "../components/ProtectedRoute";
 
 export const AppRoutes = () => {
   return (
