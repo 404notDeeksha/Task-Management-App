6f2e9c2f3cfac34fb03b0b5d2645bc863f379439
Deeksha_GitFiles <sharmadeekksha@gmail.com>
2025-02-06 17:32:06 +0530

    sidebarSlice  created

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import searchReducer from "./slices/searchSlice";
+import sidebarReducer from "./slices/sidebarSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   search: searchReducer,
+  sidebar: sidebarReducer,
 });
 
 export default rootReducer;
