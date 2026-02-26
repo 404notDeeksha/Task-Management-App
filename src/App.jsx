e9aeb18a3c2ca250c00496bd9cf935d8822d1cfb
Deeksha <sharmadeekksha@gmail.com>
2026-02-27 00:40:00 +0530

    feat (error-handling) : Wrapping error handler around App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes";
 import { Modal } from "./components/Modal";
+import { ErrorBoundaryWrapper } from "./components/ErrorBoundary";
 
 function App() {
   return (
     <Router>
-      <AppRoutes />
-      <Modal />
+      <ErrorBoundaryWrapper>
+        <AppRoutes />
+        <Modal />
+      </ErrorBoundaryWrapper>
     </Router>
   );
 }
