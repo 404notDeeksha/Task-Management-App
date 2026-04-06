f764c3bcff4fbd65f546362b63bc21730c7310e4
Deeksha <sharmadeekksha@gmail.com>
2026-04-06 19:05:59 +0530

    feat(tests): add tests

--- src/utils/fetchTasks.js
+++ src/utils/fetchTasks.js
@@ -1,7 +1,6 @@
 import { getTasks } from "../api/tasks";
 import { setAllTasks } from "../redux/slices/alltasksSlice";
 
-/* Hydrates redux with all User's Tasks in asked sort order  */
 export const fetchTasks = async (dispatch, sortBy) => {
   try {
     const result = await getTasks();
